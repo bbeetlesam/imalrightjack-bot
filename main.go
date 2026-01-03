aa3cef19b3b3cbea27cf2c3e8fc743c0be35d3be
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-01-03 15:56:17 +0700

    simple plan; just using the tgbotapi module

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOTKEN")) // use env var for early testing
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
 	fmt.Println("Hands off your stack next time.")
 }
