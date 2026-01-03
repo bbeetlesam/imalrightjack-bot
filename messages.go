06229b36fd584313d7e54f82bf8c45c90d8bb7f8
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-01-04 02:57:07 +0700

    refactor: tidy up HELP_MSG string declaration (with concatenation)

--- messages.go
+++ messages.go
@@ -2,13 +2,12 @@ package main
 
 const (
 	GREET_MSG    string = "Jack's here, mate. May I help you with those green stacks?"
-	HELP_MSG     string = `*Need help with Jack? Here are some info:*
-/start - Wake Jack's mind. You'll receive a greeting from him.
-/help - Show this messages, obviously. What are you expecting?
-/earn - Log your income. Probably the best command here.
-/spend - Log your expense. Jack hates subtraction, just saying.
-
-Got it? Now time to fully utilize Jack's simple mind.`
+	HELP_MSG     string = "*Need help with Jack? Here are some info:*\n" +
+		"/start - Wake Jack's mind. You'll receive a greeting from him.\n" +
+		"/help - Show this messages, obviously. What are you expecting?\n" +
+		"/earn - Log your income. Probably the best command here.\n" +
+		"/spend - Log your expense. Jack hates subtraction, just saying.\n\n" +
+		"*Got it?* Now time to fully utilize Jack's simple mind."
 	DEFAULT_MSG string = "Jack doesn't know that command, sadly.\nMaybe you can try to type /help."
 	START_LOG   string = "Waiting to get updates (from her)..."
 	ABOUT_MSG   string = "Who's Jack?\n\n*imalrightjack*, usually called Jack, is a bot that helps you track " +
